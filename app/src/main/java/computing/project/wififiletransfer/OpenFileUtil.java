2094013bc2cbf7d331e30514855a6f57a85cf105
zhonghui <hui450670916@outlook.com>
2021-04-10 17:14:13 +0800

    Optimize OpenFileUtil

--- app/src/main/java/computing/project/wififiletransfer/OpenFileUtil.java
+++ app/src/main/java/computing/project/wififiletransfer/OpenFileUtil.java
@@ -6,86 +6,12 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.StrictMode;
+import android.webkit.MimeTypeMap;
 import android.widget.Toast;
 
 import java.io.File;
 
-/**
- * @Description: 打开文件工具类
- * @author: ZhangYW
- * @time: 2019/1/10 10:52
- */
 public class OpenFileUtil {
-    private static final String[][] MATCH_ARRAY={
-            //{后缀名，    文件类型}
-            {".3gp",    "video/3gpp"},
-            {".apk",    "application/vnd.android.package-archive"},
-            {".asf",    "video/x-ms-asf"},
-            {".avi",    "video/x-msvideo"},
-            {".bin",    "application/octet-stream"},
-            {".bmp",      "image/bmp"},
-            {".c",        "text/plain"},
-            {".class",    "application/octet-stream"},
-            {".conf",    "text/plain"},
-            {".cpp",    "text/plain"},
-            {".doc",    "application/msword"},
-            {".docx",    "application/msword"},
-            {".xls",    "application/msword"},
-            {".xlsx",    "application/msword"},
-            {".exe",    "application/octet-stream"},
-            {".gif",    "image/gif"},
-            {".gtar",    "application/x-gtar"},
-            {".gz",        "application/x-gzip"},
-            {".h",        "text/plain"},
-            {".htm",    "text/html"},
-            {".html",    "text/html"},
-            {".jar",    "application/java-archive"},
-            {".java",    "text/plain"},
-            {".jpeg",    "image/jpeg"},
-            {".jpg",    "image/jpeg"},
-            {".js",        "application/x-javascript"},
-            {".log",    "text/plain"},
-            {".m3u",    "audio/x-mpegurl"},
-            {".m4a",    "audio/mp4a-latm"},
-            {".m4b",    "audio/mp4a-latm"},
-            {".m4p",    "audio/mp4a-latm"},
-            {".m4u",    "video/vnd.mpegurl"},
-            {".m4v",    "video/x-m4v"},
-            {".mov",    "video/quicktime"},
-            {".mp2",    "audio/x-mpeg"},
-            {".mp3",    "audio/x-mpeg"},
-            {".mp4",    "video/mp4"},
-            {".mpc",    "application/vnd.mpohun.certificate"},
-            {".mpe",    "video/mpeg"},
-            {".mpeg",    "video/mpeg"},
-            {".mpg",    "video/mpeg"},
-            {".mpg4",    "video/mp4"},
-            {".mpga",    "audio/mpeg"},
-            {".msg",    "application/vnd.ms-outlook"},
-            {".ogg",    "audio/ogg"},
-            {".pdf",    "application/pdf"},
-            {".png",    "image/png"},
-            {".pps",    "application/vnd.ms-powerpoint"},
-            {".ppt",    "application/vnd.ms-powerpoint"},
-            {".prop",    "text/plain"},
-            {".rar",    "application/x-rar-compressed"},
-            {".rc",        "text/plain"},
-            {".rmvb",    "audio/x-pn-realaudio"},
-            {".rtf",    "application/rtf"},
-            {".sh",        "text/plain"},
-            {".tar",    "application/x-tar"},
-            {".tgz",    "application/x-compressed"},
-            {".txt",    "text/plain"},
-            {".wav",    "audio/x-wav"},
-            {".wma",    "audio/x-ms-wma"},
-            {".wmv",    "audio/x-ms-wmv"},
-            {".wps",    "application/vnd.ms-works"},
-            {".xml",    "text/plain"},
-            {".z",        "application/x-compress"},
-            {".zip",    "application/zip"},
-            {"",        "*/*"}
-    };
-
     /**
      * 根据路径打开文件
      * @param context 上下文
@@ -103,12 +29,9 @@ public class OpenFileUtil {
 
         //文件的类型
         String type = "";
-        for(int i =0;i < MATCH_ARRAY.length;i++){
-            //判断文件的格式
-            if(path.contains(MATCH_ARRAY[i][0])){
-                type = MATCH_ARRAY[i][1];
-                break;
-            }
+        String extension = MimeTypeMap.getFileExtensionFromUrl(path);
+        if (extension != null) {
+            type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
         }
         try {
             File out = new File(path);
