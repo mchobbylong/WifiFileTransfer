76c8cc211e8825c989fc4cd6bffc94ff66d1368d
ZhonghuiX <hui450670916@outlook.com>
2021-05-10 12:48:29 +0800

    Add editing and saving username function

--- app/src/main/java/computing/project/wififiletransfer/MainActivity.java
+++ app/src/main/java/computing/project/wififiletransfer/MainActivity.java
@@ -1,21 +1,64 @@
 package computing.project.wififiletransfer;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.text.TextUtils;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 
+import computing.project.wififiletransfer.manager.WifiLManager;
+
 public class MainActivity extends BaseActivity {
 
     private static final int CODE_REQ_PERMISSIONS = 665;
+    private EditText username;
+    private String name;
+    private SharedPreferences sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.et_username);
+        sp=this.getPreferences(MODE_PRIVATE);
+
+        // 从SharedPreference中检索用户名
+        name=sp.getString("username", null);
+        if (name == null) {
+            name = "user" + System.currentTimeMillis();
+            SharedPreferences.Editor editor=sp.edit();
+            editor.putString("username", name);
+            editor.commit();
+        }
+        username.setText(name);
+
+        // 添加焦点监听
+        username.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus){ // 失去焦点
+                    if(TextUtils.isEmpty(((EditText)v).getText().toString().trim())){ // 输入框为空，恢复原用户名
+                        username.setText(name);
+                    }
+                    else{ // 新用户名，更新SharedPreference
+                        name = username.getText().toString();
+                        SharedPreferences.Editor editor=sp.edit();
+                        editor.putString("username", name);
+                        editor.commit();
+                    }
+                }
+            }
+        });
 
         try {
             PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_PERMISSIONS);
@@ -23,6 +66,52 @@ public class MainActivity extends BaseActivity {
             ActivityCompat.requestPermissions(this, permissions, CODE_REQ_PERMISSIONS);
         } catch (Exception ignored) {}
     }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if (isShouldHideInput(v, ev)) {
+
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                if (imm != null) {
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+            return super.dispatchTouchEvent(ev);
+        }
+        // 必不可少，否则所有的组件都不会有TouchEvent了
+        if (getWindow().superDispatchTouchEvent(ev)) {
+            return true;
+        }
+        return onTouchEvent(ev);
+    }
+
+    public boolean isShouldHideInput(View v, MotionEvent event) {
+        if (v != null && (v instanceof EditText)) {
+            int[] leftTop = {0, 0};
+            //获取输入框当前的location位置
+            v.getLocationInWindow(leftTop);
+            int left = leftTop[0];
+            int top = leftTop[1];
+            int bottom = top + v.getHeight();
+            int right = left + v.getWidth();
+            if (event.getX() > left && event.getX() < right
+                    && event.getY() > top && event.getY() < bottom) {
+                // 点击的是输入框区域，保留点击EditText的事件
+                return false;
+            } else {
+                //使EditText触发一次失去焦点事件
+                v.setFocusable(false);
+//                v.setFocusable(true); //这里不需要是因为下面一句代码会同时实现这个功能
+                v.setFocusableInTouchMode(true);
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
